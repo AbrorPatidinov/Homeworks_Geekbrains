05678b2b0931fd4e978ca940f873a669ea6f7def
Abror - Artorias <darkorb869@gmail.com>
2024-02-04 13:09:28 +0500

    Initial State

---
+++ cmake-build-debug/CMakeFiles/Homeworks_Geekbrains.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Homeworks_Geekbrains.dir/src/main.c.obj"
+  "CMakeFiles/Homeworks_Geekbrains.dir/src/main.c.obj.d"
+  "Homeworks_Geekbrains.exe"
+  "Homeworks_Geekbrains.exe.manifest"
+  "Homeworks_Geekbrains.pdb"
+  "libHomeworks_Geekbrains.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Homeworks_Geekbrains.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf_s("\n");
+
+    printf("Hello, World!\n");
+
+    printf_s("\n");
+    return EXIT_SUCCESS;
+}
